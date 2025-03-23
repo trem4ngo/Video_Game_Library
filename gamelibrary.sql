8cd1279cdbbc9fa5bf093af71115158411c5dc93
Christopher Soriano <christopher_vincent_soriano@dlsu.edu.ph>
2025-03-24 00:32:46 +0800

    Add files via upload
    User table: contact_number changed data type from INT(15) to VARCHAR(15)
    User table: Removed wallet_id
    Wallet table: Now has user_id

--- gamelibrary.sql
+++ gamelibrary.sql
@@ -15,17 +15,30 @@ USE gamelibrary;
 */
 
 -- Games Table
--- INSERT INTO Games (release_date, developer, publisher, availability, price) VALUES (...), (...);
+-- INSERT INTO Games (game_name, release_date, developer, publisher, availability, price) VALUES (...), (...);
 DROP TABLE IF EXISTS Games;
 CREATE TABLE Games (
 	game_id INT(5) AUTO_INCREMENT PRIMARY KEY,
-    game_name VARCHAR(50),
+    game_name VARCHAR(45),
     release_date DATE,
     developer VARCHAR(50),
     publisher VARCHAR(50),
     availability BOOLEAN,
-    price DECIMAL(5,2)
+    price DECIMAL(7,2)
 ) AUTO_INCREMENT = 100;
+
+-- Users Table
+-- INSERT INTO Users (first_name, last_name, birth_date, sex, contact_number, contact_email) VALUES (...), (...);
+DROP TABLE IF EXISTS Users;
+CREATE TABLE Users (
+	user_id INT(5) AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    birth_date DATE,
+    sex VARCHAR(10),
+    contact_number VARCHAR(15),
+    contact_email VARCHAR(50)
+) AUTO_INCREMENT = 6000;
 
 -- Patches Table
 -- INSERT INTO Patches (game_id, patch_title, patch_date, patch_time, patch_description) VALUES (...), (...);
@@ -50,10 +63,11 @@ CREATE TABLE REF_Genres (
 ) AUTO_INCREMENT = 2000;
 
 -- Reference Platforms Table
--- INSERT INTO REF_Platforms (platform_name) VALUES (...), (...);
+-- INSERT INTO REF_Platforms (platform_name, platform_type) VALUES (...), (...);
 DROP TABLE IF EXISTS REF_Platforms;
 CREATE TABLE REF_Platforms (
 	platform_id INT(5) AUTO_INCREMENT PRIMARY KEY,
+    platform_type VARCHAR(30),
     platform_name VARCHAR(50)
 ) AUTO_INCREMENT = 3000;
 
@@ -92,8 +106,7 @@ CREATE TABLE Transactions (
     online_url VARCHAR(100),
     transaction_status ENUM('completed','pending','failed'),
     FOREIGN KEY (game_id) REFERENCES Games(game_id),
-    FOREIGN KEY (user_id) REFERENCES Users(user_id),
-    FOREIGN KEY (payment_id) REFERENCES Payment(payment_id)
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
 ) AUTO_INCREMENT = 4000;
 
 -- Reference Payment Methods Table
@@ -106,27 +119,14 @@ CREATE TABLE REF_Methods (
     is_active BOOLEAN
 ) AUTO_INCREMENT = 5000;
 
--- Users Table
--- INSERT INTO Users (wallet_id, first_name, last_name, birth_date, sex, contact_number, contact_email) VALUES (...), (...);
-DROP TABLE IF EXISTS Users;
-CREATE TABLE Users (
-	user_id INT(5) AUTO_INCREMENT PRIMARY KEY,
-    wallet_id INT(5),
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    birth_date DATE,
-    sex VARCHAR(10),
-    contact_number INT(15),
-    contact_email VARCHAR(50),
-    FOREIGN KEY (wallet_id) REFERENCES Wallets(wallet_id)
-) AUTO_INCREMENT = 6000;
-
 -- User Wallets Table
 -- INSERT INTO Wallets (wallet_balance) VALUES (...), (...);
 DROP TABLE IF EXISTS Wallets;
 CREATE TABLE Wallets (
 	wallet_id INT(5) AUTO_INCREMENT PRIMARY KEY,
-    wallet_balance DECIMAL(9,2)
+    user_id INT(5),
+    wallet_balance DECIMAL(9,2),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
 ) AUTO_INCREMENT = 7000;
 
 -- Games Owned Table
--- gamelibrarydatasets.sql
+++ gamelibrarydatasets.sql
@@ -4,13 +4,13 @@ VALUES
 ('Strategy', 'Games that require tactical planning, resource management, and decision-making to achieve objectives.'),
 ('First Person Shooter', 'Action-packed games where players experience combat through the eyes of the protagonist, often involving firearms and fast-paced gameplay.'), 
 ('Puzzle', 'Games focused on problem-solving, logic, and pattern recognition, often featuring brain-teasing challenges.'),
-('Fighting', 'Fast-paced, one-on-one or group combat games where players use melee or martial arts techniques to defeat opponents.'),
 ('Simulation', 'Games that mimic real-life activities, such as driving, flying, farming, or managing a business, offering realistic experiences.'),
 ('Sandbox', 'Open-world games with creative freedom, allowing players to explore, build, and interact with the environment with minimal restrictions.'),
 ('Action', '‘Fast-paced games emphasizing physical challenges, combat, and reflex-based gameplay.'),
 ('Adventure', 'Story-driven games that involve exploration, puzzle-solving, and character interaction in immersive worlds.'),
-('Survival' 'Games where players must gather resources, manage health, and fend off threats to stay alive in hostile environments.'),
-('Role Playing Game', 'Games where players assume the roles of characters, progressing through quests, leveling up, and making story-altering decisions.');
+('Survival', 'Games where players must gather resources, manage health, and fend off threats to stay alive in hostile environments.'),
+('Role Playing Game', 'Games where players assume the roles of characters, progressing through quests, leveling up, and making story-altering decisions.'),
+('Sports', 'Games that simulate real-world or fictional sports, allowing players to compete in individual or team-based events, often featuring realistic physics, rules, and strategies.');
 
 -- REF_Platforms Dat Sets
 INSERT INTO REF_Platforms (platform_name, platform_type)
@@ -18,35 +18,11 @@ VALUES
 ('Microsoft Windows', 'PC'),
 ('macOS', 'PC'),
 ('Linux', 'PC'),
-('PlayStation', 'Console'),
+('PlayStation 5', 'Console'),
 ('Xbox', 'Console'),
-('Nintendo 64', 'Console'),
+('Nintendo Switch', 'Console'),
 ('iOS', 'Mobile'),
-('Android', 'Mobile'),
-('HarmonyOS', 'Mobile');
-
--- Wallet Data Sets
-INSERT INTO Wallets (wallet_balance)
-VALUES
-(5000.31),(10499.00), (8000.52), (15000.05), (768.30);
-
--- Users Data Set
-INSERT INTO Users (wallet_id, first_name, last_name, birth_date, sex, contact_number, contact_email)
-VALUES
-('Francisco', 'Manuel', 2003-09-30, 'Male', 09515429802, 'christophersori@gmail.com'), 
-('Erika', 'Chua', 1999-05-23, 'Female', 09175125832, 'erika_chua@yahoo.com'), 
-('Maximus', 'Sotelo', 2001-04-30, 'Male', 09231926518, 'maximo19@gmail.com'),  
-('Christian', 'Tan', 2006-01-20, 'Male', 09175475253, 'christiantan_30@gmail.com'),
-('Marian', 'Salazar', 2000-03-23, 'Female', 09228275693, 'salazar_marian@outlook.com');
-
--- Games Data Set
-INSERT INTO Games (game_name, release_date, developer, publisher, price)
-VALUES
-('Left 4 Dead 2', 2009-11-17, 'Valve', 'Valve', 335.00),
-('Genshin Impact', 2020-09-28, 'miHoYo Co., Ltd.', 'miHoYo Co., Ltd.', 0.00),
-('Minecraft: Bedrock Edition', 2016-11-18, 'Mojang Studios', 'Microsoft Studios', 1375.00),
-('NBA 2k24', 2023-09-08, 'Visual Concepts', '2K Games', 2199.00),
-('Dota 2', 9013-07-09, 'Valve', 'Valve', 0.00);
+('Android', 'Mobile');
 
 INSERT INTO REF_Methods (method_name, method_description, is_active)
 VALUES
@@ -57,37 +33,67 @@ VALUES
 ('GCash', 'Mobile Wallet payment service in the Philippines', true),
 ('Maya (PH)', 'Mobile Wallet payment service in the Philippines', true);
 
--- One by One Patches Insertion
-SELECT game_id FROM Games WHERE game_name = 'Left 4 Dead 2';
-INSERT INTO Patches (game_id, patch_title, patch_date, patch_time, patch_description)
+-- Games Data Set
+INSERT INTO Games (game_name, release_date, developer, publisher, price)
 VALUES
-(game_id, '2.2.4.3', 2024-12-03, '03:55:00', 'An update has been released for Left 4 Dead 2. 
-- Fixed some exploits used to crash remote servers.'),
-(game_id, '2.2.4.2', 2024-10-25, '04:58:00', 'Fixed several crash and remote code execution exploits.
-Fixed a crash on some NVIDIA graphics cards when shader detail is set to High.');
+('Left 4 Dead 2', '2009-11-17', 'Valve', 'Valve', 335.00),
+('Genshin Impact', '2020-09-28', 'miHoYo Co., Ltd.', 'miHoYo Co., Ltd.', 0.00),
+('Minecraft: Bedrock Edition', '2016-11-18', 'Mojang Studios', 'Microsoft Studios', 1375.00),
+('NBA 2k24', '2023-09-08', 'Visual Concepts', '2K Games', 2199.00),
+('Dota 2', '2013-07-09', 'Valve', 'Valve', 0.00);
 
-SELECT game_id FROM Games WHERE game_name = 'Genshin Impact';
-INSERT INTO Patches (game_id, patch_title, patch_date, patch_time, patch_description)
+-- Users Data Set
+INSERT INTO Users (first_name, last_name, birth_date, sex, contact_number, contact_email)
 VALUES
-(game_id, 'Moonlight Amidst Dreams', 2025-02-12, '06:00:00', 'New Characters: Embrace of Enchanting Dreams" and Yumemizuki Mizuki (5-Star Anemo Catalyst).
+('Francisco', 'Manuel', '2003-09-30', 'Male','09515429802', 'christophersori@gmail.com'), 
+('Erika', 'Chua', '1999-05-23', 'Female', '09175125832', 'erika_chua@yahoo.com'), 
+('Maximus', 'Sotelo', '2001-04-30', 'Male', '09231926518', 'maximo19@gmail.com'),  
+('Christian', 'Tan', '2006-01-20', 'Male', '09175475253', 'christiantan_30@gmail.com'),
+('Marian', 'Salazar', '2000-03-23', 'Female', '09228275693', 'salazar_marian@outlook.com');
+
+-- Wallet Data Sets
+INSERT INTO Wallets (wallet_balance, user_id)
+VALUES (5000.31,(SELECT user_id FROM Users WHERE first_name = 'Francisco' LIMIT 1));
+
+INSERT INTO Wallets (wallet_balance, user_id)
+VALUES (12700.25,(SELECT user_id FROM Users WHERE first_name = 'Erika' LIMIT 1));
+
+INSERT INTO Wallets (wallet_balance, user_id)
+VALUES (748.00,(SELECT user_id FROM Users WHERE first_name = 'Maximus' LIMIT 1));
+
+INSERT INTO Wallets (wallet_balance, user_id)
+VALUES (2089.13,(SELECT user_id FROM Users WHERE first_name = 'Christian' LIMIT 1));
+
+INSERT INTO Wallets (wallet_balance, user_id)
+VALUES (14328.15,(SELECT user_id FROM Users WHERE first_name = 'Marian' LIMIT 1));
+
+-- One by One Patches Insertion
+INSERT INTO Patches (game_id, patch_title, patch_date, patch_time, patch_description)
+VALUES ((SELECT game_id FROM Games WHERE game_name = 'Left 4 Dead 2'), '2.2.4.3', '2024-12-03', '03:55:00', 
+'An update has been released for Left 4 Dead 2. - Fixed some exploits used to crash remote servers.'),
+((SELECT game_id FROM Games WHERE game_name = 'Left 4 Dead 2'), '2.2.4.2', '2024-10-25', '04:58:00', 
+'Fixed several crash and remote code execution exploits.Fixed a crash on some NVIDIA graphics cards when shader detail is set to High.');
+
+INSERT INTO Patches (game_id, patch_title, patch_date, patch_time, patch_description)
+VALUES((SELECT game_id FROM Games WHERE game_name = 'Genshin Impact'), 'Moonlight Amidst Dreams', '2025-02-12', '06:00:00', 
+'New Characters: Embrace of Enchanting Dreams" and Yumemizuki Mizuki (5-Star Anemo Catalyst).
 New Weapons: Sunny Morning Sleep-In (5-Star Catalyst) and Tamayuratei no Ohanashi (4-Star Polearm).
 Event Wishes: Phase I - Dawns Drifting Reverie (Yumemizuki Mizuki) and Romaritime Meluserenity (Sigewinne) &
 Phase II - Chanson of Many Waters (Furina)and Tempestuous Destiny (Wriothesley).'),
-(game_id, 'Incandescent Ode of Resurrection', 2025-01-01, '06:00:00', 'New Characters: "Night-Igniting Flame" Mavuika (5-Star Pyro Claymore),
+((SELECT game_id FROM Games WHERE game_name = 'Genshin Impact'), 'Incandescent Ode of Resurrection', '2025-01-01', '06:00:00', 
+'New Characters: "Night-Igniting Flame" Mavuika (5-Star Pyro Claymore),
 "Obsidian Opalstar" Citlali (5-Star Cryo Catalyst), and "Spring Woven From Jade" Lan Yan (4-Star Anemo Catalyst).
 New Weapons: A Thousand Blazing Suns (5-Star Claymore) and Starcallers Watch (5-Star Catalyst).
 Event Wishes: Phase I - Ancient Flame Ablaze (Mavuika) and Starry Nights Whispers (Citlali)
-Phase II - The Hearths Ashen Shadow (Arlecchino & Lan Yan), Illuminating Lightning (Clorinde & Lan Yan),
-Remembrance of Jade and Stone (Chronicled Wish)');
+Phase II - The Hearths Ashen Shadow (Arlecchino & Lan Yan)');
 
-SELECT game_id FROM Games WHERE game_name = 'Minecraft';
 INSERT INTO Patches (game_id, patch_title, patch_date, patch_time, patch_description)
-VALUES
-(game_id, '1.21.62', 2025-02-25, 'Gameplay: Fixed an issue where attacking a mob while sprint jumping would remove all player momentum (MCPE-189501) 
+VALUES ((SELECT game_id FROM Games WHERE game_name = 'Minecraft'), '1.21.62', '2025-02-25', 
+'Gameplay: Fixed an issue where attacking a mob while sprint jumping would remove all player momentum (MCPE-189501) 
 Fixed an issue where laggy players fighting would occasionally not be able to hit each other (MCPE-190552), 
 Fixed an issue where Player collision box would sometimes be incorrect after using a bed (MCPE-190604)'),
-(game_id, '1.21.51', 2024-12-11, 'General: Fixed several crashes that could occur during gameplay, 
-Fixed issue where some shaped recipes were not parsing properly, 
+((SELECT game_id FROM Games WHERE game_name = 'Minecraft'), '1.21.51', '2024-12-11', 
+'General: Fixed several crashes that could occur during gameplay, Fixed issue where some shaped recipes were not parsing properly, 
 Fixed issue where legacy data driven block did not apply the render layer from the material instances component');
 
 SELECT game_id FROM Games WHERE game_name = 'NBA 2k24';
@@ -139,4 +145,99 @@ INSERT INTO Games_Owned (user_id, game_id) VALUES (user_id, games_id);
 
 -- Game_Genres Data Sets
 SELECT game_id FROM Games WHERE game_name = 'NBA 2k24';
-SELECT genre_id FROM Game_Genres
+SELECT genre_id FROM Game_Genres WHERE genre_name = 'Sports';
+INSERT INTO Game_Genres (game_id, genre_id) VALUES (game_id, genre_id);
+SELECT genre_id FROM Game_Genres WHERE genre_name = 'Simulation';
+INSERT INTO Game_Genres (game_id, genre_id) VALUES (game_id, genre_id);
+SELECT genre_id FROM Game_Genres WHERE genre_name = 'Adventure';
+INSERT INTO Game_Genres (game_id, genre_id) VALUES (game_id, genre_id);
+
+SELECT game_id FROM Games WHERE game_name = 'Left 4 Dead 2';
+SELECT genre_id FROM Game_Genres WHERE genre_name = 'First Person Shooter';
+INSERT INTO Game_Genres (game_id, genre_id) VALUES (game_id, genre_id);
+SELECT genre_id FROM Game_Genres WHERE genre_name = 'Survival';
+INSERT INTO Game_Genres (game_id, genre_id) VALUES (game_id, genre_id);
+SELECT genre_id FROM Game_Genres WHERE genre_name = 'First Person Shooter';
+INSERT INTO Game_Genres (game_id, genre_id) VALUES (game_id, genre_id);
+
+SELECT game_id FROM Games WHERE game_name = 'Minecraft';
+SELECT genre_id FROM Game_Genres WHERE genre_name = 'Sandbox';
+INSERT INTO Game_Genres (game_id, genre_id) VALUES (game_id, genre_id);
+SELECT genre_id FROM Game_Genres WHERE genre_name = 'Adventure';
+INSERT INTO Game_Genres (game_id, genre_id) VALUES (game_id, genre_id);
+SELECT genre_id FROM Game_Genres WHERE genre_name = 'Survival';
+INSERT INTO Game_Genres (game_id, genre_id) VALUES (game_id, genre_id);
+SELECT genre_id FROM Game_Genres WHERE genre_name = 'Simulation';
+INSERT INTO Game_Genres (game_id, genre_id) VALUES (game_id, genre_id);
+
+SELECT game_id FROM Games WHERE game_name = 'Dota 2';
+SELECT genre_id FROM Game_Genres WHERE genre_name = 'Action';
+INSERT INTO Game_Genres (game_id, genre_id) VALUES (game_id, genre_id);
+SELECT genre_id FROM Game_Genres WHERE genre_name = 'Strategy';
+INSERT INTO Game_Genres (game_id, genre_id) VALUES (game_id, genre_id);
+
+SELECT game_id FROM Games WHERE game_name = 'Genshin Impact';
+SELECT genre_id FROM Game_Genres WHERE genre_name = 'Adventure';
+INSERT INTO Game_Genres (game_id, genre_id) VALUES (game_id, genre_id);
+SELECT genre_id FROM Game_Genres WHERE genre_name = 'Role Playing Game';
+INSERT INTO Game_Genres (game_id, genre_id) VALUES (game_id, genre_id);	
+SELECT genre_id FROM Game_Genres WHERE genre_name = 'Puzzle';
+INSERT INTO Game_Genres (game_id, genre_id) VALUES (game_id, genre_id);	
+
+-- Game_Platforms Data Sets 
+SELECT game_id FROM Games WHERE game_name = 'Left 4 Dead 2';
+SELECT platform_id FROM ref_platforms WHERE platform_name = 'Steam';
+INSERT INTO Game_Platforms (game_id, platform_id) VALUES (game_id, platform_id);
+SELECT platform_id FROM ref_platforms WHERE platform_name = 'macOS';
+INSERT INTO Game_Platforms (game_id, platform_id) VALUES (game_id, platform_id);
+SELECT platform_id FROM ref_platforms WHERE platform_name = 'Linux';
+INSERT INTO Game_Platforms (game_id, platform_id) VALUES (game_id, platform_id);
+SELECT platform_id FROM ref_platforms WHERE platform_name = 'Microsoft Windows';
+INSERT INTO Game_Platforms (game_id, platform_id) VALUES (game_id, platform_id);
+SELECT platform_id FROM ref_platforms WHERE platform_name = 'Xbox';
+INSERT INTO Game_Platforms (game_id, platform_id) VALUES (game_id, platform_id);
+
+SELECT game_id FROM Games WHERE game_name = 'Genshin Impact';
+SELECT platform_id FROM ref_platforms WHERE platform_name = 'Microsoft Windows';
+INSERT INTO Game_Platforms (game_id, platform_id) VALUES (game_id, platform_id);
+SELECT platform_id FROM ref_platforms WHERE platform_name = 'Playstation 5';
+INSERT INTO Game_Platforms (game_id, platform_id) VALUES (game_id, platform_id);
+SELECT platform_id FROM ref_platforms WHERE platform_name = 'Android';
+INSERT INTO Game_Platforms (game_id, platform_id) VALUES (game_id, platform_id);
+SELECT platform_id FROM ref_platforms WHERE platform_name = 'iOS';
+INSERT INTO Game_Platforms (game_id, platform_id) VALUES (game_id, platform_id);
+SELECT platform_id FROM ref_platforms WHERE platform_name = 'Xbox 360';
+INSERT INTO Game_Platforms (game_id, platform_id) VALUES (game_id, platform_id);
+
+SELECT game_id FROM Games WHERE game_name = 'Minecraft';
+SELECT platform_id FROM ref_platforms WHERE platform_name = 'Microsoft Windows';
+INSERT INTO Game_Platforms (game_id, platform_id) VALUES (game_id, platform_id);
+SELECT platform_id FROM ref_platforms WHERE platform_name = 'macOS';
+INSERT INTO Game_Platforms (game_id, platform_id) VALUES (game_id, platform_id);
+SELECT platform_id FROM ref_platforms WHERE platform_name = 'iOS';
+INSERT INTO Game_Platforms (game_id, platform_id) VALUES (game_id, platform_id);
+SELECT platform_id FROM ref_platforms WHERE platform_name = 'Android';
+INSERT INTO Game_Platforms (game_id, platform_id) VALUES (game_id, platform_id);
+SELECT platform_id FROM ref_platforms WHERE platform_name = 'PlayStation 5';
+INSERT INTO Game_Platforms (game_id, platform_id) VALUES (game_id, platform_id);
+SELECT platform_id FROM ref_platforms WHERE platform_name = 'Xbox 360';
+INSERT INTO Game_Platforms (game_id, platform_id) VALUES (game_id, platform_id);
+SELECT platform_id FROM ref_platforms WHERE platform_name = 'Linux';
+INSERT INTO Game_Platforms (game_id, platform_id) VALUES (game_id, platform_id);
+
+SELECT game_id FROM Games WHERE game_name = 'NBA 2k24';
+SELECT platform_id FROM ref_platforms WHERE platform_name = 'Microsoft Windows';
+INSERT INTO Game_Platforms (game_id, platform_id) VALUES (game_id, platform_id);
+SELECT platform_id FROM ref_platforms WHERE platform_name = 'PlayStation 5';
+INSERT INTO Game_Platforms (game_id, platform_id) VALUES (game_id, platform_id);
+SELECT platform_id FROM ref_platforms WHERE platform_name = 'Nintendo Switch';
+INSERT INTO Game_Platforms (game_id, platform_id) VALUES (game_id, platform_id);
+
+SELECT game_id FROM Games WHERE game_name = 'Dota 2';
+SELECT platform_id FROM ref_platforms WHERE platform_name = 'Microsoft Windows';
+INSERT INTO Game_Platforms (game_id, platform_id) VALUES (game_id, platform_id);
+SELECT platform_id FROM ref_platforms WHERE platform_name = 'macOS';
+INSERT INTO Game_Platforms (game_id, platform_id) VALUES (game_id, platform_id);
+SELECT platform_id FROM ref_platforms WHERE platform_name = 'Linux';
+
+-- Reviews Data Sets
